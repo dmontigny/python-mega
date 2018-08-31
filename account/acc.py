59e74c780507e475157cadc4f77dbc5929a1cb9e
dmonty <dmontigny@yahoo.com>
2018-08-30 21:27:09 -0400

    Added inheritance to account OOP project

--- account/acc.py
+++ account/acc.py
@@ -1,5 +1,6 @@
 
 class Account:
+    """This class is a generic account class"""
     def __init__(self, filepath):
         self.filepath = filepath
         with open(filepath, 'r') as file:
@@ -17,6 +18,9 @@ class Account:
             file.write(str(self.balance))
 
 class Checking(Account):
+    """This class generates checking account objects"""
+    type = 'checking'
+
     def __init__(self, filepath, fee):
         self.filepath = filepath
         self.fee = fee
@@ -25,7 +29,13 @@ class Checking(Account):
     def transfer(self, amount):
         self.balance -= amount + self.fee
 
-checking = Checking('balance.txt', 1)
-checking.transfer(110)
-checking.commit()
-print(checking.balance)
+jack_checking = Checking('jack.txt', 1)
+jack_checking.transfer(110)
+jack_checking.commit()
+print(jack_checking.balance)
+
+john_checking = Checking('john.txt', 1)
+john_checking.transfer(110)
+john_checking.commit()
+print(john_checking.balance)
+print(john_checking.__doc__)
