2ea0e4fa42f1745a97b04d9a71724ab1b2f629a8
dmontigny <dmontigny27@gmail.com>
2019-12-11 20:03:31 -0500

    working on web scraping - pasco arrests

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -1,2 +1,8 @@
 # udemy course: The Python Mega Course: Build 10 Real World Applications
 
+
+
+
+
+# r = requests.get("http://www.pyclass.com/example.html", headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
+
